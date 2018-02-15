78a79c8b08f825eae30ec4e068ca3a601903e4cc
Abigail Steckel <abigailsteckel@students.berkeley.net>
2018-02-15 11:46:05 -0800

    figuring it out

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-ZangPzzdN3AzHFVjbqglw
+
+// https://github.com/harvardartmuseums/api-docs
+
+$.ajax({
+    url: 'https://api.harvardartmuseums.org/Object?apikey=b58976d0-1287-11e8-b9d0-8dfce0d04c25',
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function() { alert('Failed!'); }
+
+
+});
